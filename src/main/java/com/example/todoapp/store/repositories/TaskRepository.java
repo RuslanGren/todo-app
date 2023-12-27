638a8d13ecae33379d1e476e74801e9cb3cbf0f3
Ruslan <gren.ruslan@gmail.com>
2023-12-27 21:47:40 +0200

    add

--- src/main/java/com/example/todoapp/store/repositories/TaskRepository.java
+++ src/main/java/com/example/todoapp/store/repositories/TaskRepository.java
@@ -1,9 +1,9 @@
 package com.example.todoapp.store.repositories;
 
-import com.example.todoapp.store.entities.ProjectEntity;
+import com.example.todoapp.store.entities.TaskEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProjectRepository extends JpaRepository<ProjectEntity, Long> {
+public interface TaskRepository extends JpaRepository<TaskEntity, Long> {
 }
--- src/main/java/com/example/todoapp/store/repositories/TaskStateRepository.java
+++ src/main/java/com/example/todoapp/store/repositories/TaskStateRepository.java
@@ -1,9 +1,9 @@
 package com.example.todoapp.store.repositories;
 
-import com.example.todoapp.store.entities.ProjectEntity;
+import com.example.todoapp.store.entities.TaskStateEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProjectRepository extends JpaRepository<ProjectEntity, Long> {
+public interface TaskStateRepository extends JpaRepository<TaskStateEntity, Long> {
 }
